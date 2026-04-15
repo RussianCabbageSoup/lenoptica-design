b3d544eeeba9632b2d2c270fca86973c4011e9be
RussianCabbageSoup <testylya333@gmail.com>
2026-04-15 03:05:49 +0300

    adaptive about section

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ import { initCarousel } from "./carousel.js";
 document.addEventListener('DOMContentLoaded', () => {
     initCarousel();
 });
+
+function replaceImage() {
+    const img = document.querySelector('.about__images-img img');
+    if (window.innerWidth <= 750) {
+        img.src = 'images/png/header-img.png';
+    } else {
+        img.src = 'images/png/about.png';
+    }
+}
+
+window.addEventListener('resize', replaceImage);
+window.addEventListener('load', replaceImage);
